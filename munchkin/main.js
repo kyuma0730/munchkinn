b904e69121ddc4cb1922f8457f5ecf18f2ffa46c
kyuma0730 <67457763+kyuma0730@users.noreply.github.com>
2020-07-01 00:39:40 +0900

    initial commit
    munchkin

---
+++ munchkin/main.js
@@ -0,0 +1,21 @@
+
+$(function(){
+  // #で始まるリンクをクリックしたら実行されます
+  $('a[href^="#"]').click(function() {
+    // スクロールの速度
+    var speed = 400; // ミリ秒で記述
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+
+  $('aside').click(function(){
+    $('input[type="checkbox"]').removeAttr('checked').prop('checked', false).change();
+  });
+  $('a').click(function(){
+    $('input[type="checkbox"]').removeAttr('checked').prop('checked', false).change();
+  });
+
+});
